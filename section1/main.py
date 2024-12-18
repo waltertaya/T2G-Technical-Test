2ac3e96547de962d994aaa1e5f0d78b391adeff7
waltertaya <waltermittybecker5@gmail.com>
2024-12-18 11:42:25 +0300

    Add docstrings to main.py for improved code documentation

--- section1/main.py
+++ section1/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+''' Program to capitalize the first letter of each word in a given string. '''
 
 def capitalize_1st_letter(s):
+    ''' Function to capitalize the first letter of each word in a given string. '''
     result = ''
     for string in s.split():
         result += string.capitalize() + ' '
@@ -8,6 +10,7 @@ def capitalize_1st_letter(s):
 
 
 if __name__ == '__main__':
+    ''' Main function to test the above function. '''
     example = ['hello', 'i love programming', 'python is easy']
     for i in example:
         print(f'Input: {i}')
